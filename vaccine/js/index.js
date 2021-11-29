1446aa1bab5ad3ae8b6bc79f1e10158fe8437d8f
MRHJuwel <86875582+MRHJuwel@users.noreply.github.com>
2021-11-29 14:29:14 +0600

    vaccine Presentation
    only for presentation

---
+++ vaccine/js/index.js
@@ -0,0 +1,12 @@
+ (function($) {
+         $(document).ready(function() {
+                        
+               
+    $('.pie-title-center"').pieChart({
+        barColor: 'red',
+        
+    });
+            
+
+      }),
+    })(jQuery) 
